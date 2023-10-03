3bf1c2c50802ebb7d6ac6706c0cd0076d81a77db
Daniel Isla <danielislacanales96@gmail.com>
2023-10-03 19:15:43 -0300

    fix about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,28 @@
-"use client"
-
+// External libraries
 import { Amplify } from 'aws-amplify';
-import awsconfig from '../../aws-exports';
-import '@aws-amplify/ui-react/styles.css';
-import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 import { Button } from '@nextui-org/button';
+
+// Internal imports
+import awsconfig from '../../aws-exports';
 import { Title, Bold } from "@tremor/react";
 import AreaChartComponent from '@/src/components/area_chart_component';
+
+// Styles
+import '@aws-amplify/ui-react/styles.css';
+
+// Amplify configuration
 Amplify.configure(awsconfig);
 
-export function AboutPage({ signOut, user }: WithAuthenticatorProps) {
+function AboutPage({ signOut, user }: WithAuthenticatorProps) {
 	return (
-	<div>
-        <Bold className="block w-full">Hola {user?.attributes?.email}</Bold>
-        <Title className="block w-full mt-2">Esta es la página de About</Title>
-        <Button className="block w-full mt-2" color="secondary" onClick={signOut}>Sign out</Button>
-    </div>
+		<div>
+			<Bold className="block w-full">Hola {user?.attributes?.email}</Bold>
+			<Title className="block w-full mt-2">Esta es la página de About</Title>
+			<Button className="block w-full mt-2" color="secondary" onClick={signOut}>Sign out</Button>
+		</div>
 	);
-  }
-  
-  export default withAuthenticator(AboutPage);
+}
+
+export default withAuthenticator(AboutPage);
